7869d51cb07c3bdce1fb8388d4d4c51a15f4211c
陳信屹 <ossug.hychen@gmail.com>
2017-12-24 22:09:21 +0800

    Revert "first zh_TW i18n commit."
    This reverts commit 37f9ddb0f2ba5604dbcc77a187c5a5b053d8b5d0.

--- scripts/compile-mo.sh
+++
@@ -1,2 +0,0 @@
-msgfmt -o locale/zh-Hant-TW/LC_MESSAGES/messages.mo \
-      locale/zh-Hant-TW/LC_MESSAGES/messages.po
--- scripts/update-po.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pybabel extract -F babel.cfg -o h/locale/h.pot h/**/**/*
-pybabel update -D h -l zh_TW -d h/locale -i h/locale/h.pot
-pybabel compile -D h -f -d h/locale

69dc224323be1979def4f86e535c808648288d47
陳信屹 <ossug.hychen@gmail.com>
2017-12-26 18:24:19 +0800

    Dirty hack for fixing translation does not work.

--- h/i18n.py
+++ h/i18n.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
-from pyramid.i18n import TranslationStringFactory
+from pyramid.i18n import make_localizer, TranslationString as __
 
-TranslationString = TranslationStringFactory('h')
+#@FIXME: dirty hack for fixing translation does not  work.
+import os
+local_dir = os.path.dirname(os.path.abspath(__file__)) + '/locale'
+loc = make_localizer('zh_TW', [local_dir])
+
+def TranslationString(msg):
+    return loc.translate(__(msg, domain='h'))

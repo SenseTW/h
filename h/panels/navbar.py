5b122ca15939367f9c3112292c9d4ff01ee2c5cb
Pomin Wu <pomin5@gmail.com>
2018-03-20 15:22:36 +0800

    Revert "Revert "Removed Developer page""
    This reverts commit 0541f6bc3c420b3c192e60b79e6e5041e53500b1.

--- h/panels/navbar.py
+++ h/panels/navbar.py
@@ -49,7 +49,7 @@ def navbar(context, request, search=None, opts=None):
             {'title': _('Account details'), 'link': request.route_url('account')},
             {'title': _('Edit profile'), 'link': request.route_url('account_profile')},
             {'title': _('Notifications'), 'link': request.route_url('account_notifications')},
-            {'title': _('Developer'), 'link': request.route_url('account_developer')},
+            # {'title': _('Developer'), 'link': request.route_url('account_developer')},
         ],
         'signout_item': {'title': _('Sign out'), 'link': request.route_url('logout')},
         'groups_menu_items': groups_menu_items,
